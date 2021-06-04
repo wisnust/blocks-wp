1a31a66585684088685335739148cce6dc280c1a
Wisnu Satrio T <wisnu21cool@gmail.com>
2021-06-04 20:37:17 +0700

    Slider Post block styling and responsive mobile tablet

--- js/custom.js
+++ js/custom.js
@@ -1,2 +1,19 @@
 jQuery(document).ready(function ($) {
+
+    // Slider Post
+
+    // Internal Link Carousel Slider
+    $('.slider-post-row').each(function(i, el) {
+        $(this).slick({
+            infinite: false,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: false,
+            arrows: true,
+            adaptiveHeight: true,
+            prevArrow: '<button class="slide-prev btn-arrow"><i class="far fa-arrow-left"></i></button>',
+            nextArrow: '<button class="slide-next btn-arrow"><i class="far fa-arrow-right"></i></button>',
+        });
+    });
+    
 });
